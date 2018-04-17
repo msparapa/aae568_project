13159fd43ff51bf7cbcfd6ed4aef2a4eb0fe6d89
Unknown <wzaidi@applieddefense.com>
2018-04-16 22:31:03 -0400

    Added more description to the filter. Unsuccessful getting the UKF to work so using Unscented Transform and Ensemble Kalman Filter for now.

---
+++ code/EnKF.m
@@ -0,0 +1,55 @@
+function [x_update] = EnKF(h,x_initial,w,z,obs,num_iterations) 
+
+[~,num_members]     = size(x_initial);
+p1                  = size(x_initial,1);
+m1                  = size(obs,1);
+xvec                = [];
+x_update_vec        = [];
+yvec                = [];
+x_estimate          = x_initial;
+
+Zcov = eye(m1);     % Create measurement noise covariance matrix
+
+for j = 1:m1
+  Zcov(j,j) = z(j)^2;
+end
+
+for i = 1:num_iterations  
+ 
+   for j = 1:num_members
+     % Create noise 
+     W(:,j)          = w.*randn(p1,1);       % State Process Noise, not using here and will add random acceleration in 'accel_polar.m' instead        
+     Z(:,j)          = z.*randn(m1,1);       % Measurement Noise           
+     
+     % Forecast Step
+     forecastState   = x_estimate(:,j) + W;  % Replaced with already propagated sigma points                          
+     meas            = obs + Z(:,j);         % 2-dimensional [r,rDot] vector                   
+     forecastMeas    = h(forecastState)';    % Mapping from measurement space to dynamics space (which is the same in our case)        
+      
+     % Store estimates and measurements
+     y(:,j)          = meas;                 
+     y_forecast(:,j) = forecastMeas; 
+     
+   end
+   
+   % Analysis Step
+   x_estimatebar     = mean(x_estimate,2);   % Replaced with propagated sigma points
+   ybar              = mean(y,2);            % Mean of measurement return
+   y_forecastbar     = mean(y_forecast,2);   % Mean of predicted measurement
+
+   for j = 1:p1
+     Ex(j,:) = [x_estimate(j,:) - x_estimatebar(j)];  % State covariance
+   end
+
+   for j = 1:m1
+     Ey(j,:) = [y_forecast(j,:) - y_forecastbar(j)];  % Measurement covariance
+   end
+
+   Pxy            = Ex*Ey'/(num_members - 1);         % Cross-correlation
+   Pyy            = Ey*Ey'/(num_members - 1) + Zcov;  % Innovation  
+   K              = Pxy*inv(Pyy);                     % Kalman Gain
+   x_update       = x_estimate + K*(y - y_forecast);  % State Update  
+   if i == num_iterations
+       x_estimatebar = mean(x_estimate,2);
+   end
+end
--- code/ukf.m
+++ code/ukf.m
@@ -1,4 +1,4 @@
-function [x_update, x_update_vec] = ukf(h,x_initial,w,z,obs,num_iterations) 
+function [x_update] = ukf(h,x_initial,w,z,obs,num_iterations,Wm,Wc) 
 
 [~,num_members]     = size(x_initial);
 p1                  = size(x_initial,1);
@@ -8,7 +8,7 @@ x_update_vec        = [];
 yvec                = [];
 x_estimate          = x_initial;
 
-Zcov = eye(m1);     % create measurement noise covariance matrix
+Zcov = eye(m1);     % Create measurement noise covariance matrix
 
 for j = 1:m1
   Zcov(j,j) = z(j)^2;
@@ -18,39 +18,37 @@ for i = 1:num_iterations
  
    for j = 1:num_members
      % Create noise 
-     W(:,j)          = w.*randn(p1,1);                
-     Z(:,j)          = z.*randn(m1,1);                 
+     W(:,j)          = w.*randn(p1,1);       % State Process Noise, not using here and will add random acceleration in 'accel_polar.m' instead        
+     Z(:,j)          = z.*randn(m1,1);       % Measurement Noise           
      
      % Forecast Step
-     forecastState   = x_estimate(:,j) + W;                             
-     meas            = obs + Z(:,j);                           
-     forecastMeas    = h(forecastState)';              
+     forecastState   = x_estimate(:,j) + W;  % Replaced with already propagated sigma points                          
+     meas            = obs + Z(:,j);         % 2-dimensional [r,rDot] vector                   
+     forecastMeas    = h(forecastState)';    % Mapping from measurement space to dynamics space (which is the same in our case)        
       
      % Store estimates and measurements
-     y(:,j)          = meas;
+     y(:,j)          = meas;                 
      y_forecast(:,j) = forecastMeas; 
      
    end
    
    % Analysis Step
-   x_estimatebar     = mean(x_estimate,2);    
-   ybar              = mean(y,2);          
-   y_forecastbar     = mean(y_forecast,2);  
+   x_estimatebar     = mean(x_estimate,2);   % Replaced with propagated sigma points
+   ybar              = mean(y,2);            % Mean of measurement return
+   y_forecastbar     = mean(y_forecast,2);   % Mean of predicted measurement
 
    for j = 1:p1
-     Ex(j,:) = [x_estimate(j,:) - x_estimatebar(j)];  
+     Ex(j,:) = [x_estimate(j,:) - x_estimatebar(j)];  % State covariance
    end
 
    for j = 1:m1
-     Ey(j,:) = [y_forecast(j,:) - y_forecastbar(j)];  
+     Ey(j,:) = [y_forecast(j,:) - y_forecastbar(j)];  % Measurement covariance
    end
 
-   Pxy            = Ex*Ey'/(num_members - 1);
-   Pyy            = Ey*Ey'/(num_members - 1) + Zcov;   
-   K              = Pxy*inv(Pyy);
-   x_update       = x_estimate + K*(y - y_forecast);
-   x_update_vec   = [x_update_vec x_estimatebar];        
-   yvec           = [yvec ybar];
+   Pxy            = Ex*Ey'/(num_members - 1);         % Cross-correlation
+   Pyy            = Ey*Ey'/(num_members - 1) + Zcov;  % Innovation  
+   K              = Pxy*inv(Pyy);                     % Kalman Gain
+   x_update       = x_estimate + K*(y - y_forecast);  % State Update  
    if i == num_iterations
        x_estimatebar = mean(x_estimate,2);
    end
