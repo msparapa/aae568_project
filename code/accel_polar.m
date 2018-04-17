a2fb3aaf4ebb3ca4d5c130970564ca9a4e2b2f57
Andrew Cox <andrewdcox10@gmail.com>
2018-04-17 10:01:59 -0400

    Adding documentation to report

--- code/accel_polar.m
+++ code/accel_polar.m
@@ -5,7 +5,9 @@ theta     = f( 2);
 rDot      = f( 3);
 thetaDot  = f( 4);
 
-rDotDot = r*thetaDot^2 - mu/r^2*(1-3/2*J2*(Re/r)^2) + T/m*(cos(alpha)*cos(theta)+sin(alpha)*sin(theta));
-thetaDotDot = -2*rDot*thetaDot/r + T/m/r*(sin(alpha)*cos(alpha)-cos(alpha)*sin(theta));
+rDotDot = r*thetaDot^2 - mu/r^2*(1-3/2*J2*(Re/r)^2) +...
+    T/m*(cos(alpha)*cos(theta)+sin(alpha)*sin(theta));
+thetaDotDot = -2*rDot*thetaDot/r +...
+    T/m/r*(sin(alpha)*cos(alpha)-cos(alpha)*sin(theta));
 
 dfdt = [rDot; thetaDot; rDotDot; thetaDotDot; -T/Isp/g0];
--- code/eoms_truth.m
+++ code/eoms_truth.m
@@ -14,6 +14,8 @@ function xdot = eoms_truth(t, x, p)
 %       u_disc  -   Discretized control history (1D, contains the angle,
 %                   alpha)
 %       t_disc  -   Discretized time values associated with u_disc
+%       sigX    -   2x1 vector of noise standard deviations to scale a 
+%                   gaussian noise addition to the accelerations
 %
 %   Author: Andrew Cox
 %   Version: April 16, 2018
@@ -22,14 +24,14 @@ function xdot = eoms_truth(t, x, p)
 xdot = zeros(size(x));
 xdot(1:2) = x(3:4);
 
-mu = params.mu;
-T = params.T;
+mu = p.mu;
+T = p.T;
 
 % Determine mass based on current time
-m = params.m0 - T/(params.Isp * params.g0) * t;
+m = p.m0 - T/(p.Isp * p.g0) * t;
 
 % Determine state based on the discretized state and the current time
-u = interpState(params.u_disc, params.t_disc, t);
+u = interpState(p.u_disc, p.t_disc, t);
 
 % r_ddot
 xdot(3) = x(1)*x(4)^2 + mu/(x(1)^2) + (T/m)*(cos(u)*cos(x(2)) + sin(u)*sin(x(2)));
@@ -37,4 +39,6 @@ xdot(3) = x(1)*x(4)^2 + mu/(x(1)^2) + (T/m)*(cos(u)*cos(x(2)) + sin(u)*sin(x(2))
 % theta_ddot
 xdot(4) = -2*x(3)*x(4)/x(1) + (T/(x(1)*m))*(sin(u)*cos(x(2)) - cos(u)*sin(x(2)));
 
+% Add random noise to the accelerations
+xdot(3:4) = xdot(3:4) + p.sigX.*randn(4,1);
 end
