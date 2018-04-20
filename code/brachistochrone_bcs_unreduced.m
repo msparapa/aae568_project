be560cfa571f3fe291f6a29b69defe8aea495d64
Mike <msparapa@purdue.edu>
2018-04-20 11:47:57 -0400

    Ref #3 Added direct method support to bvpmc

--- code/brachistochrone_bcs_unreduced.m
+++ code/brachistochrone_bcs_unreduced.m
@@ -1,4 +1,4 @@
-function [BC] = brachistochrone_bcs_unreduced(t0, X0, tf, Xf, quads0, quadsf, params, consts)
+function [BC] = brachistochrone_bcs_unreduced(t0, X0, u0, tf, Xf, uf, quads0, quadsf, params, consts)
 
 x0 = X0(1);
 y0 = X0(2);
@@ -15,16 +15,17 @@ lvf = Xf(6);
 tf = params(1);
 g = consts(1);
 
-theta0 = brachistochrone_control_unreduced(t0, X0, params, consts);
-thetaf = brachistochrone_control_unreduced(tf, Xf, params, consts);
+% theta0 = brachistochrone_control_unreduced(t0, X0, params, consts);
+% thetaf = brachistochrone_control_unreduced(tf, Xf, params, consts);
+
+theta0 = u0;
+thetaf = uf;
 
 BC(1) = x0;
 BC(2) = y0;
 BC(3) = v0;
 BC(4) = xf - 1;
 BC(5) = yf + 1;
-BC(6) = lvf;
-BC(7) = (-g * lvf * sin(thetaf) + lxf * vf * cos(thetaf) + lyf * vf * sin(thetaf) + 1);
 
 end
 
--- code/brachistochrone_main.m
+++ code/brachistochrone_main.m
@@ -12,6 +12,8 @@ vguess = linspace(0, 2.5, options.Nodes);
 lxguess = -0.1*linspace(1, 1, options.Nodes);
 lyguess = 0.1*linspace(1, 1, options.Nodes);
 lvguess = -0.1*linspace(1, 0, options.Nodes);
+uguess = 0*linspace(0, 0, options.Nodes);
+
 tfguess = 0.5;
 T = linspace(0, 1, options.Nodes);
 solinit0 = bvpinit(T, [0,0,0,0,0,0], tfguess);
@@ -22,12 +24,17 @@ solinit0.y(4,:) = lxguess;
 solinit0.y(5,:) = lyguess;
 solinit0.y(6,:) = lvguess;
 solinit0.consts = [g];
+solinit0.control(1,:) = uguess;
 
-x0_reduced = [solinit0.y(3,:), solinit0.y(6,:), tfguess, xguess(1), yguess(1), lxguess(1), lyguess(1)];
+options.isdirect = 1;
 
 tic;
 sol = bvpmc(@brachistochrone_eoms_unreduced, [], @brachistochrone_bcs_unreduced, solinit0, options);
 time0 = toc;
 fprintf('M time : \t %.4f\n', time0);
 
+figure();
+subplot(2,1,1);
 plot(sol.y(1,:), sol.y(2,:));
+subplot(2,1,2);
+stairs(sol.control);
